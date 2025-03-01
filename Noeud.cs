0e5c4eff62e77ff4478f8fa87ddd8b0b52731932
Aztral1 <ywmoysan@gmail.com>
2025-03-01 17:00:43 +0100

    Update Noeud.cs

--- Noeud.cs
+++ Noeud.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections.Generic;
-/// Représente un nœud dans un graphe.
-public class Noeud
-{
-    /// Identifiant unique du nœud.
-    public int IdNoeud { get; set; }
-    /// Liste des nœuds adjacents.
-    public List<Noeud> Adjacents { get; set; } = new List<Noeud>();
-    /// Constructeur pour créer un nœud avec un identifiant donné.
-    public Noeud(int idnoeud)
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivinParisVfinale
+{   /// Représente un nœud dans un graphe.
+    public class Noeud
     {
-        IdNoeud = idnoeud;
+        /// Identifiant unique du nœud.
+        public int IdNoeud { get; set; }
+        /// Liste des nœuds adjacents.
+        public List<Noeud> Adjacents { get; set; } = new List<Noeud>();
+        /// Constructeur pour créer un nœud avec un identifiant donné.
+        public Noeud(int idnoeud)
+        {
+            IdNoeud = idnoeud;
+        }
     }
 }
