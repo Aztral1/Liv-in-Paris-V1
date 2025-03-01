447d65ff2bf59a9ef6ea7ea460078fce56dfb9c3
Aztral1 <ywmoysan@gmail.com>
2025-03-01 09:46:37 +0100

    Update Noeud.cs

--- Noeud.cs
+++ Noeud.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
-
+using System.Collections.Generic;
+/// Représente un nœud dans un graphe.
 public class Noeud
 {
-    public int Id { get; set; }
+    /// Identifiant unique du nœud.
+    public int IdNoeud { get; set; }
+    /// Liste des nœuds adjacents.
     public List<Noeud> Adjacents { get; set; } = new List<Noeud>();
-
-    public Noeud(int id)
+    /// Constructeur pour créer un nœud avec un identifiant donné.
+    public Noeud(int idnoeud)
     {
-        Id = id;
+        IdNoeud = idnoeud;
     }
 }
